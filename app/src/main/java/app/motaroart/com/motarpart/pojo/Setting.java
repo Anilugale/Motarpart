1a7b275c0f43a3400c86c65264ca295332e0b59d
Aanil1989 <Anilugale1989@gmail.com>
2015-01-11 22:15:01 +0530

    From home 11 jan

---
+++ app/src/main/java/app/motaroart/com/motarpart/pojo/Setting.java
@@ -0,0 +1,53 @@
+package app.motaroart.com.motarpart.pojo;
+
+public class Setting
+{
+    private String DisplayText;
+
+    private String SettingId;
+
+    private String KeyValue;
+
+    private String KeyName;
+
+    public String getDisplayText ()
+    {
+        return DisplayText;
+    }
+
+    public void setDisplayText (String DisplayText)
+    {
+        this.DisplayText = DisplayText;
+    }
+
+    public String getSettingId ()
+    {
+        return SettingId;
+    }
+
+    public void setSettingId (String SettingId)
+    {
+        this.SettingId = SettingId;
+    }
+
+    public String getKeyValue ()
+    {
+        return KeyValue;
+    }
+
+    public void setKeyValue (String KeyValue)
+    {
+        this.KeyValue = KeyValue;
+    }
+
+    public String getKeyName ()
+    {
+        return KeyName;
+    }
+
+    public void setKeyName (String KeyName)
+    {
+        this.KeyName = KeyName;
+    }
+}
+
